8fe95a347a65801563af39b65912335cf9f420a6
Pujan <pujan3892@gmail.com>
2019-02-28 00:43:10 -0600

    Project Scaffold

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client/dist/index.html"))
+);
+app.listen(port, () => console.log("server running...."));
