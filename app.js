9ec5a6c50187a7bcee98d61e6b341f89ce7bff8e
Pujan <pujan3892@gmail.com>
2019-04-03 19:45:41 -0500

    heroku mods add

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(morgan("dev"));
