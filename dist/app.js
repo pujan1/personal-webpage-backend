b509b10f3cc1776d25092966b7957d952b87c000
Pujan <pujan3892@gmail.com>
2019-04-24 22:30:29 -0500

    dist

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./dist/dist")));
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./dist/dist/index.html")));
+app.listen(port, () => console.log("server running...."));
